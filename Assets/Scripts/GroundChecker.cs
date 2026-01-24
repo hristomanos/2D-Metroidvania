80f9c2fac3981f4f292742d8d7cd411ff1588b61
Christomanos Anastasiou <hristoma@gmail.com>
2026-01-24 23:07:15 +0000

    Add GroundChecker

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    [Header("Ground check")]
+    [SerializeField] private LayerMask groundLayerMask;
+    [SerializeField] float groundCheckCircleRadius;
+    
+    private GroundCheckDebugRenderer groundCheckGizmoRenderer;
+    private bool isOnGround;
+    
+    public bool IsOnGround => isOnGround;
+
+    private void Awake()
+    {
+        groundCheckGizmoRenderer = GetComponent<GroundCheckDebugRenderer>();
+        groundCheckGizmoRenderer.Initialize(groundCheckCircleRadius);
+    }
+    
+    private void FixedUpdate()
+    {
+        GroundCheck();
+    }
+    
+    private void GroundCheck()
+    {
+        var circleOffset = 0.4f;
+
+        var circlePosition = transform.position + Vector3.down * circleOffset;
+
+        isOnGround = Physics2D.OverlapCircle(circlePosition, groundCheckCircleRadius, groundLayerMask);
+    }
+}
