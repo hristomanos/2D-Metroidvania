b1c7d24497d986745937e71228709c69ca0d8748
Christomanos Anastasiou <hristoma@gmail.com>
2026-01-24 23:41:52 +0000

    Update PlayerMovementController with latest changes

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -6,11 +6,8 @@ public class PlayerMovementController : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private float movementSpeed;
     
-    private static readonly int AttackUp = Animator.StringToHash("AttackUp");
-    private static readonly int AttackDown = Animator.StringToHash("AttackDown");
-    
     private SpriteRenderer spriteRenderer;
-    private Animator animator;
+    private Rigidbody2D rb;
     
     private PlayerJump playerJump;
     private GroundChecker groundChecker;
@@ -18,28 +15,15 @@ public class PlayerMovementController : MonoBehaviour
     private PlayerInput playerInput;
     private Vector2     movementInput;
     private InputAction moveAction;
-    private InputAction attackAction;
-    
-    private Rigidbody2D rb;
-    
-    private ComboAttackInvoker comboAttackInvoker;
     
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         playerInput = GetComponent<PlayerInput>();
         playerJump = GetComponent<PlayerJump>();
-        comboAttackInvoker = GetComponent<ComboAttackInvoker>();
         
-        moveAction = playerInput.actions["Move"];
-        attackAction = playerInput.actions["Attack"];
-    }
-
-    private void Start()
-    {
-        attackAction.performed += OnAttackButtonPressed;
+        moveAction = playerInput.actions[PlayerInputStrings.Move];
     }
 
     private void Update()
@@ -51,20 +35,12 @@ public class PlayerMovementController : MonoBehaviour
     {
         FlipSprite();
         
-        if (comboAttackInvoker.IsComboAttacking)
-            StopHorizontalMovement();
-        else
-            MoveHorizontally();
+        MoveHorizontally();
     }
     
     private void MoveHorizontally()
     {
         rb.linearVelocity = new Vector2(movementInput.x * movementSpeed, Mathf.Max(rb.linearVelocity.y, -playerJump.MaxFallSpeed));
-    }
-
-    private void StopHorizontalMovement()
-    {
-        rb.linearVelocity = Vector2.zero;
     }
     
     private void FlipSprite()
@@ -72,22 +48,6 @@ public class PlayerMovementController : MonoBehaviour
         if(spriteRenderer.flipX && movementInput.x > 0f || spriteRenderer.flipX is false && movementInput.x < 0f)
         {
             spriteRenderer.flipX = !spriteRenderer.flipX;
-        }
-    }
-
-    private void OnAttackButtonPressed(InputAction.CallbackContext context)
-    {
-        if(movementInput.y > 0.5f)
-        {
-            animator.SetTrigger(AttackUp);
-        }
-        else if(movementInput.y < -0.5f)
-        {
-            animator.SetTrigger(AttackDown);
-        }
-        else
-        {
-            comboAttackInvoker.HandleCombo();
         }
     }
 }
