dcc5cd6706c3c4b579728f69cd25023313177a52
Christomanos Anastasiou <hristoma@gmail.com>
2026-01-24 22:34:13 +0000

    Refactor PlayerMovementController.

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -3,63 +3,42 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    private static readonly int AttackUp = Animator.StringToHash("AttackUp");
-    private static readonly int AttackDown = Animator.StringToHash("AttackDown");
-
     [Header("Attributes")]
     [SerializeField] private float movementSpeed;
-    [SerializeField] private float jumpForce;
-
-    [Header("Ground check")]
-    [SerializeField] private LayerMask groundLayerMask;
-    [SerializeField] float groundCheckCircleRadius;
-
-    [Header("Gravity modifier")]
-    [SerializeField] private int baseGravity;
-    [SerializeField] private int maxFallSpeed;
-    [SerializeField] private int fallSpeedModifier;
-
-    [SerializeField] ComboAttackInvoker comboAttackInvoker;
-
+    
+    private static readonly int AttackUp = Animator.StringToHash("AttackUp");
+    private static readonly int AttackDown = Animator.StringToHash("AttackDown");
+    
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    
+    private PlayerJump playerJump;
+    private GroundChecker groundChecker;
+    
     private PlayerInput playerInput;
-    private InputAction jumpAction;
+    private Vector2     movementInput;
     private InputAction moveAction;
     private InputAction attackAction;
-    private InputAction attackUpAction;
-
+    
     private Rigidbody2D rb;
-
-    private bool isOnGround;
-    private Vector2 movementInput;
-
-    public bool IsOnGround => isOnGround;
-
-    SpriteRenderer spriteRenderer;
-
-    Animator animator;
-
-    GroundCheckDebugRenderer groundCheckGizmoRenderer;
-
+    
+    private ComboAttackInvoker comboAttackInvoker;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
-        playerInput = GetComponent<PlayerInput>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        groundCheckGizmoRenderer = GetComponent<GroundCheckDebugRenderer>();
-        groundCheckGizmoRenderer.Initialize(groundCheckCircleRadius);
+        playerInput = GetComponent<PlayerInput>();
+        playerJump = GetComponent<PlayerJump>();
+        comboAttackInvoker = GetComponent<ComboAttackInvoker>();
         
-        jumpAction = playerInput.actions["Jump"];
         moveAction = playerInput.actions["Move"];
         attackAction = playerInput.actions["Attack"];
     }
 
-    void Start()
+    private void Start()
     {
-        jumpAction.performed += OnJumpButtonPressed;
-        jumpAction.canceled += OnJumpButtonReleased;
         attackAction.performed += OnAttackButtonPressed;
     }
 
@@ -68,68 +47,26 @@ public class PlayerMovementController : MonoBehaviour
         movementInput = moveAction.ReadValue<Vector2>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         FlipSprite();
-
-        GroundCheck();
         
         if (comboAttackInvoker.IsComboAttacking)
             StopHorizontalMovement();
         else
             MoveHorizontally();
-
-        if(isOnGround)
-        {
-            ResetGravityModifier();   
-        }
     }
-
-    private void OnJumpButtonPressed(InputAction.CallbackContext context)
-    {
-        if(isOnGround is false)
-            return;
-
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);        
-    }
-
-    private void OnJumpButtonReleased(InputAction.CallbackContext context)
-    {
-        if(isOnGround)
-            return;
-
-        ApplyGravityModifier();
-    }
-
+    
     private void MoveHorizontally()
     {
-        rb.linearVelocity = new Vector2(movementInput.x * movementSpeed, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
+        rb.linearVelocity = new Vector2(movementInput.x * movementSpeed, Mathf.Max(rb.linearVelocity.y, -playerJump.MaxFallSpeed));
     }
 
     private void StopHorizontalMovement()
     {
         rb.linearVelocity = Vector2.zero;
     }
-
-    private void ApplyGravityModifier()
-    {
-        rb.gravityScale = fallSpeedModifier;
-    }
-
-    private void ResetGravityModifier()
-    {
-        rb.gravityScale = baseGravity;
-    }
-
-    private void GroundCheck()
-    {
-        var circleOffset = 0.4f;
-
-        var circlePosition = transform.position + Vector3.down * circleOffset;
-
-        isOnGround = Physics2D.OverlapCircle(circlePosition, groundCheckCircleRadius, groundLayerMask);
-    }
-
+    
     private void FlipSprite()
     {
         if(spriteRenderer.flipX && movementInput.x > 0f || spriteRenderer.flipX is false && movementInput.x < 0f)
@@ -140,9 +77,6 @@ public class PlayerMovementController : MonoBehaviour
 
     private void OnAttackButtonPressed(InputAction.CallbackContext context)
     {
-        if (isOnGround is false)
-            return;
-        
         if(movementInput.y > 0.5f)
         {
             animator.SetTrigger(AttackUp);
